9eaedb9a447177a73604057d1761c996276d39b9
bekturdievashaydo <bekturdiyevashaydo9@gmail.com>
2024-06-07 17:51:46 +0500

    add fdilxd

---
+++ asus.py
@@ -0,0 +1,267 @@
+
+
+# taomlar={'akam':'somsa','opom':'osh','man':'lagmon','katta singlim':'kabob','kichik singlim':'fast food'}
+# print(taomlar)
+# del taomlar['kichik singlim']
+# del taomlar['man']
+# print(taomlar)
+#
+#
+#
+# dictionary={'if':'agar','book':'kitob',}
+# print(dictionary)
+#
+#
+#
+# lugat=dictionary.get('from','buday soz mavjud emas'
+# print(lugat)
+#
+#
+# #
+# oila_azosi={'ism':'man bekturdieva shaydo','t_yil':'2008','manzil':'Urganch'}
+# print(f"{oila_azosi['ism'].title()},\
+# {oila_azosi['t_yil']} - yilda tug'ilgan,\
+# {oila_azosi['manzil']} - da yashaydi")
+# oila_azosi={'ism':'dadam Qadirov Ozod','t_yil':'1983','manzil':'Urganch'}
+# print(f"{oila_azosi['ism'].title()},\
+# {oila_azosi['t_yil']} - yilda tug'ilgan,\
+# {oila_azosi['manzil']} - da yashaydi")
+#
+# oila_azosi={'ism':' onam Aminobva marifat','t_yil':'1989','manzil':'Urganch'}
+# print(f"{oila_azosi['ism'].title()},\
+# {oila_azosi['t_yil']} - yilda tug'ilgan,\
+# {oila_azosi['manzil']} - da yashaydi")
+#
+# oila_azosi={'ism':' ukam qadirova sabina','t_yil':'2013','manzil':'Urganch'}
+# print(f"{oila_azosi['ism'].title()},\
+# {oila_azosi['t_yil']} - yilda tug'ilgan,\
+# {oila_azosi['manzil']} - da yashaydi")
+#
+# oila_azosi={'ism':'ukam qadirova elvira','t_yil':'2017','manzil':'Urganch'}
+# print(f"{oila_azosi['ism'].title()},\
+# {oila_azosi['t_yil']} - yilda tug'ilgan,\
+# {oila_azosi['manzil']} - da yashaydi")
+#
+#
+#
+# # #
+# # #
+# # talaba_0={'ism':'jonibek uralov',
+# #           'familia':'uralov'}
+# #
+# #  for kalit,qiymat in talaba_0.items():
+# #      print(f"kalit: {kalit}")
+#      print(f"qiymAT:{qiymat} \n")
+
+#
+#
+# telefonlar = {
+#     'ali':'iphone x',
+#     'vali':'galaxy s9',
+#     'olim':'mi 10 pro',
+# #     'orif':'nokia 3310'
+#     }
+#
+# for k, q in telefonlar.items():
+#     print(f"{k.title()}ning telefoni {q}")
+# #
+# mahsulotlar = { # Do'kondagi mahsulotlar
+#     'olma':10000,
+#     'anor':20000,
+#     'uzum':40000,
+#     'anjir':25000,
+#     'shaftoli':30000
+#     }
+#
+# print(mahsulotlar.keys())
+#
+# print('Do\'kondagi mahsulotlar:')
+# for mahsulot in mahsulotlar.keys():
+#     print(mahsulot.title())
+# bozorlik = ['anor','uzum','non','baliq']
+# for mahsulot in mahsulotlar:
+#     if mahsulot in bozorlik:
+#         print(f"{mahsulot.title()} {mahsulotlar[mahsulot]} so'm")
+#
+#
+#
+# for buyum in bozorlik:
+#     if buyum not in mahsulotlar:
+#         print(f"Iltimos, do'koningizga {buyum} ham olib keling")
+#
+#
+#
+#
+# print("Do'konimizdagi mahsulotlar:")
+# for mahsulot in sorted(mahsulotlar):
+#     print(mahsulot.title())
+#    'shaxnnoza':'honor X5',
+#     'shaydo':'redmi 12',
+#     '#
+# guruh={
+# dilnura':'honor X 7',
+#     'elvira':'mi 11 lite',
+#     'kamol':'IPhone 14 pro max',
+#     'jasur':'IPhone X',
+#     int(telefonlar.values())
+#         print('Foydalanuvchilar quyidagi telefonlarni ishlatishadi:')
+# for tel in telefonlar.values()
+# print(tel)
+#     'zilola':'galaxy A52',
+
+ # telefonlar = {
+ #         'ali': 'iphone x',
+ #         'vali': 'galaxy s9',
+ #         'olim': 'mi 10 pro',
+ #         'orif': 'nokia 3310',
+ #         'hamida': 'galaxy s9',
+ #         'maryam': 'huawei p30',
+ #         'tohir': 'iphone x',
+ #         'umar': 'iphone x'
+ #     }
+#
+# print('Foydalanuvchilar quyidagi telefonlarni ishlatishadi:')
+# for tel in  set (telefonlar.values()):
+#        'charos':'galaxy A52',
+#     'chinora':'honor X 7',}
+
+# #
+# # print('ular wu tel ni ishlatishadi:')
+# # for  oquvchi in set(guruh.values()):
+# #     print(oquvchi)
+# ism=input('oquvchilar qanda  tel ishlatishadi?')
+# if ism in guruh.keys():
+#     print(guruh
+# davlat={'AQSH':'Washington D.C:','ITALIYA':'Rim','MALAYZIYA':'Kuala Lumpur','UZBEKISTAN':'Toshkent','ROSSIYA':'Moskva',}
+# for kalit,qiymat in davlat.items():
+#     print(f"kalit:{kalit}")
+#     print(f"qiymat:{qiymat}")
+# print('bu davlatning poytaxti:')
+# for viloyat in set (davlatlar
+# for k,q in davlat.items():
+#     print
+#(
+# pri
+# for davlat in sorted({qiymat}):
+# #     print(qiymat.title())
+# #
+# # # #
+# # # menular={'ice latte':15000,'pizza':70000,'osh':20000,'shashlik':6000,'spagetti':25000,}
+# # # print(menular.keys())
+# # # print('restarandagi mahsulotlar:')
+# # # for menu in menular.keys():
+# # #     print (menu.title())
+# # # restaran_menusi=['turk qahvasi','lavash','pizza','ice latte']
+# # # for menu in menular:
+# # #     if menu in restaran_menusi:
+# # #         print(f"{menu.title()} {menular[menu]} som")
+# # # for taom in restaran_menusi:
+# # #     if taom not in restaran_menusi:
+# # #         print(f"bizda {taom} yoq")
+# # #
+# # #
+# # #
+# # #
+# # #f
+# car0 = {
+# 'model':'lacetti',
+# 'rang': 'oq',
+# 'yil': 2018,
+# 'narh': 13000,
+# 'km': 50000,
+# 'korobka': 'avtomat'
+# }
+#
+#
+# car1 = {
+#     'model': 'nexia 3',
+#     'rang': 'qora',
+#     'yil': 2015,
+#     'narh': 9000,
+#     'km': 89000,
+#     'korobka': 'mexanika'
+# }
+#
+# car2 = {
+#     'model': 'gentra',
+#     'rang': 'qizil',
+#     'yil': 2019,
+#     'narh': 15000,
+#     'km': 20000,
+#     'korobka': 'mexanika'
+# }
+# #
+# # car=car0
+# # print(f"{car['model'].title()}"), \
+# #     {car['rang']}
+# cars=[car0,car1,car2]
+# for car in cars:
+#     print(f"{car['model'].title()},\
+#           {car['rang']} rang'\
+#           {car['yil']}-yil,{car['narh']}$")
+# print(cars[2])
+
+
+# malibus=[]
+# for n in range(10):
+#     new_car={'model':'malibu',
+#          'rang':'none'}
+#     malibus.append(new_car)
+# for malibu in malibus[:3]:
+#     malibu['rang']='qizil'
+# for malibu in malibus[3:6]:
+#     malibu['rang'] = 'qoro'
+# print(malibu)
+#
+#
+# for malibu in malibus[:3]:
+#     malibu['rang']='qizil'
+# for malibu in malibus[3:6]:
+#     malibu['korobka'] = 'mexanika'
+#     print(malibu)
+#
+#
+#
+# if malibu in malibus:
+#     if malibu['korobka']=='avto'
+#         malibu['narx']
+#
+# dasturchilar={'ali':['phyton','C++'],
+#               'vali':['html','css']}
+# for ism,tillar in dasturchilar.items():9-2-
+#     print(f"\n{ism.title()}quyidagi dasturlash tillarini biladi:")
+#     for til in tillar:
+
+
+kino1={
+    'shaydo':['samoviy ishq', "vfghghjgh", "fgfhft"],
+    'uzuklar hukumdori',
+    'garri poter',
+}
+kino2={
+    'shaydo':'20-asr qizi',
+    'hayr yer',
+}
+
+kinolar=[kino1,kino2]
+for kino in kinolar:
+    print(f"[{}")
+
+
+
+
+
+
+
+
+
+davlat={
+    'uzbekistan':{'hududi':'448978 kv.km',
+    'aholisi':'33000000',}
+for
+
+
+
+
+
+
